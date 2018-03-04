4722ea4b56b1462a9d1b624238445c530c005813
Olena Penn <olenapenn@gmail.com>
2018-03-03 15:18:32 -1000

    square divs have ids, may or may not be helpful

--- game/scripts/app.js
+++ game/scripts/app.js
@@ -1,7 +1,23 @@
 //make board
 //make token 1
 //make token 2
-console.log("Hello");
+
+const spots = [
+  // [],
+  // [],
+  // [],
+  // [],
+  // [],
+  // [],
+  // [],
+  // [],
+];
+
+//check if is array since console will only return as object, this took forever to figure out though Im sure it was mentioned in class. cool.
+console.log(typeof spots);
+const isSpotsArr = $.isArray(spots);
+console.log(isSpotsArr);
+
 $(() => {
 
   let clicks = 0;
@@ -29,12 +45,49 @@ $(() => {
 
 };
 
+// const $boardArr = [];
+
   const $board = $("<div>").addClass("board");
   $("body").append($board);
+  // $boardArr.push($board);
+  console.log(typeof $board);
+  // console.log($("div"));
+  // console.log($("div").eq(2).eq(0));
+  // console.log(typeof $("div").eq(2).eq(0));
+  // const isBoardArr = $.isArray($boardArr);
+  // console.log(isBoardArr);
+  // console.log($boardArr);
+  // console.log($boardArr[0]);
+  // console.log(($boardArr[0][0]));
+
 
     for (let i = 0; i <42; i++){
-    const $square = $("<div>").addClass("square");
+    const $square = $("<div>")
+    $square.attr("id",  1+i);
+    $square.addClass("square");
     $board.append($square);
+    console.log(typeof $square);
+
+  // const $squareArr = [];
+  // $squareArr.push($square);
+  // const isSquareArr = $.isArray($squareArr);
+  // console.log(isSquareArr);
+  // console.log($squareArr);
+  // console.log($squareArr[0]);
+  // console.log($squareArr[0][1]);
+
+
+
+
+
+
+    //
+    // $boardArr.push($square);
+    // console.log($boardArr);
+    // console.log(typeof $square);
+    // const isArr = $.isArray($boardArr);
+    // console.log(isArr);
+
 
 
 
