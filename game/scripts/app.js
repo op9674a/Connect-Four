364b76fe59b5fd2eb125cef2b772d0e24be95399
Olena Penn <olenapenn@gmail.com>
2018-03-03 20:47:28 -1000

    can log if spot played by odd or even

--- game/scripts/app.js
+++ game/scripts/app.js
@@ -4,6 +4,7 @@
 //make array of squares
 //check if is array since console will only return as object.
 //access individual squares
+//only allow square on lowest available space in column
 //make array of played squares?
 //check if played squares = win...somehow
 const $squareArr=[];
@@ -12,7 +13,7 @@ $(() => {
 
   let clicks = 0;
 
-  const placeIcon = (event) => {
+  const placeIconBottomRow = (event) => {
 
     clicks ++;
 
@@ -24,33 +25,52 @@ $(() => {
     $icon2.addClass("O").text("O");
     console.log($icon2);
 
+    // if (true) {
+    //
+    // }
     if (clicks %2 !== 0){
       $(event.currentTarget).empty();
       $(event.currentTarget).append($icon1);
+      $(event.currentTarget).removeClass().addClass("playedOdd");
+      const $playedOdd = [];
+      const $trackPlayedOdd = $playedOdd.push($(event.currentTarget).hasClass("playedOdd"));
+      console.log($playedOdd);
     }
     else if (clicks %2 === 0){
       $(event.currentTarget).empty();
       $(event.currentTarget).append($icon2);
-    }
+      $(event.currentTarget).removeClass().addClass("playedEven");
 
-};
+      const $playedEven = [];
+      const $trackPlayedEven = $playedEven.push($(event.currentTarget).hasClass("playedEven"));
+      console.log($playedEven);
+    }
+  };
+
 const $board = $("<div>").addClass("board");
 $("body").append($board);
 
 
-    for (let i = 0; i <42; i++){
+    for (let i = 0; i <20; i++){
     const $square = $("<div>");
     $square.attr("id", 0+i);
     $square.addClass("square");
     $board.append($square);
 
     const $makeSquareArr = $squareArr.push($square);
-    // console.log(typeof $squareArr);
-    // const isSquareArr = $.isArray($squareArr);
-    // console.log(isSquareArr)
 
-  $square.one("click", placeIcon);
+
+  $square.one("click", placeIconBottomRow);
+
+
 };
-  console.log($squareArr[0]);
-  console.log($squareArr[1]);
+  // console.log($squareArr[0]);
+  // console.log($squareArr[1]);
+
+
+
+  const horizontalWin = () => {
+  //   if playedSquares[] =
+  }
+
 });
