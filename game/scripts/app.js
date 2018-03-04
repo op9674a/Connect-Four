08c6e47433f926ddcadd041dda2e566507cc3c22
Olena Penn <olenapenn@gmail.com>
2018-03-03 22:10:05 -1000

    lay out rows, columns, and diagonals

--- game/scripts/app.js
+++ game/scripts/app.js
@@ -19,11 +19,13 @@ $(() => {
 
     const $icon1 = $("<div>");
     $icon1.addClass("X").text("X");
+    // .attr("src" ,"https://www.teacherswithapps.com/wp-content/uploads/2017/11/download-1.png");
     console.log($icon1);
 
     const $icon2 = $("<div>");
     $icon2.addClass("O").text("O");
     console.log($icon2);
+
 
     // if (true) {
     //
@@ -31,20 +33,27 @@ $(() => {
     if (clicks %2 !== 0){
       $(event.currentTarget).empty();
       $(event.currentTarget).append($icon1);
-      $(event.currentTarget).removeClass().addClass("playedOdd");
+      $(event.currentTarget).removeClass().addClass("Odd");
       const $playedOdd = [];
-      const $trackPlayedOdd = $playedOdd.push($(event.currentTarget).hasClass("playedOdd"));
+      const $trackPlayedOdd = $playedOdd.push($(event.currentTarget).hasClass("Odd"));
       console.log($playedOdd);
     }
     else if (clicks %2 === 0){
       $(event.currentTarget).empty();
       $(event.currentTarget).append($icon2);
-      $(event.currentTarget).removeClass().addClass("playedEven");
+      $(event.currentTarget).removeClass().addClass("Even");
 
       const $playedEven = [];
-      const $trackPlayedEven = $playedEven.push($(event.currentTarget).hasClass("playedEven"));
+      const $trackPlayedEven = $playedEven.push($(event.currentTarget).hasClass("Even"));
       console.log($playedEven);
     }
+    // const horizontalWin = () => {
+    //   if ($("16").hasClass("Even") && $("17").hasClass("Even") && $("18").hasClass("Even") && $("19").hasClass("Even")){
+    //     alert("Even wins!")
+    //   }
+    // }
+    // horizontalWin();
+
   };
 
 const $board = $("<div>").addClass("board");
@@ -58,19 +67,39 @@ $("body").append($board);
     $board.append($square);
 
     const $makeSquareArr = $squareArr.push($square);
-
-
-  $square.one("click", placeIconBottomRow);
-
-
+$square.one("click", placeIconBottomRow);
 };
-  // console.log($squareArr[0]);
-  // console.log($squareArr[1]);
+// ROWS
+const $row1 = [$squareArr[16], $squareArr[17], $squareArr[18], $squareArr[19]];
+
+const $row2 = [$squareArr[12], $squareArr[13], $squareArr[14], $squareArr[15]];
+
+const $row3 = [$squareArr[8], $squareArr[9], $squareArr[10], $squareArr[11]];
+
+const $row4 = [$squareArr[4], $squareArr[5], $squareArr[6], $squareArr[7]];
+
+const $row5 = [$squareArr[0], $squareArr[1], $squareArr[2], $squareArr[3]];
+
+// COLUMNS
+const $column1 = [$squareArr[0], $squareArr[4], $squareArr[8], $squareArr[12], $squareArr[16]];
+
+const $column2 = [$squareArr[1], $squareArr[5], $squareArr[9], $squareArr[13], $squareArr[17]];
+
+const $column3 = [$squareArr[2],$squareArr[6],$squareArr[10],$squareArr[14],$squareArr[18]];
+
+const $column4 = [$squareArr[3],$squareArr[7],$squareArr[11],$squareArr[15],$squareArr[19]];
+
+//DIAGONAL
+const $diag1 = [$squareArr[0],$squareArr[5],$squareArr[10],$squareArr[15]];
+
+const $diag2 = [$squareArr[3],$squareArr[6],$squareArr[9],$squareArr[12]];
+
+const $diag3 = [$squareArr[4],$squareArr[9],$squareArr[14],$squareArr[19]];
+
+const $diag4 = [$squareArr[7],$squareArr[10],$squareArr[13],$squareArr[16]];
 
 
-
-  const horizontalWin = () => {
-  //   if playedSquares[] =
-  }
 
 });
+  // console.log($squareArr[0]);
+  // console.log($squareArr[1]);
