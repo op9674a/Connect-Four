ae50206b65efc969aa421aafe7e98f315c6f34d2
Olena Penn <olenapenn@gmail.com>
2018-03-05 10:00:31 -1000

    column 0 placement

--- game/scripts/app.js
+++ game/scripts/app.js
@@ -25,53 +25,84 @@ const $squareArr=[];
 $(() => {
 
 let clicks = 0;
+let columnClicks = 0;
 
     const placeIcon = (event) => {
 
     clicks ++;
+    columnClicks++;
 
     const $icon1 = $("<div>");
-    $icon1.addClass("X").text("X");
+    $icon1.addClass("X");
+
+
       // console.log($icon1);
 
     const $icon2 = $("<div>");
-    $icon2.addClass("O").text("O");
+    $icon2.addClass("O");
       // console.log($icon2);
 
-        // if ($(event.currentTarget) !== lowest available spot {
-        //   place Token on lowest available spot
-        // }
 
-    if (clicks %2 !== 0) {
-    $(event.currentTarget).empty();
-    $(event.currentTarget).append($icon1);
-    $(event.currentTarget).removeClass().addClass("Odd");
+
+    if (clicks %2 !== 0 && columnClicks %2 !==0) {
+      console.log($($(".square" + "column" + 0)[7]));
+      if($($(".square" + "column" + 0)[7-columnClicks]).hasClass("square")){
+        $($(".square" + "column" + 0)[7-columnClicks]).empty();
+        $($(".square" + "column" + 0)[7-columnClicks]).append($icon1);
+        $($(".square" + "column" + 0)[7-columnClicks]).removeClass().addClass("Odd");
+
+      }
+
+    // $(event.currentTarget).empty();
+    // $(event.currentTarget).append($icon1);
+    // $(event.currentTarget).removeClass().addClass("Odd");
 
     const $playedOdd = [];
     const $trackPlayedOdd = $playedOdd.push($(event.currentTarget).hasClass("Odd"));
     console.log($playedOdd + "odd");
     }
-    else if (clicks %2 === 0){
-    $(event.currentTarget).empty();
-    $(event.currentTarget).append($icon2);
-    $(event.currentTarget).removeClass().addClass("Even");
+    else if (clicks %2 === 0 && columnClicks %2==0) {
+        console.log($($(".square" + "column" + 0)[7]));
+        if($($(".square" + "column" + 0)[7-columnClicks]).hasClass("square")){
+          $($(".square" + "column" + 0)[7-columnClicks]).empty();
+          $($(".square" + "column" + 0)[7-columnClicks]).append($icon2);
+          $($(".square" + "column" + 0)[7-columnClicks]).removeClass().addClass("Even");
+
+        }
+
+    // $(event.currentTarget).empty();
+    // $(event.currentTarget).append($icon2);
+    // $(event.currentTarget).removeClass().addClass("Even");
 
     const $playedEven = [];
     const $trackPlayedEven = $playedEven.push($(event.currentTarget).hasClass("Even"));
     console.log($playedEven + "even");
     }
+
   };
 
     const $board = $("<div>").addClass("board");
     $("body").append($board);
 
+
     for (let i = 0; i <42; i++){
     const $square = $("<div>");
     $square.attr("id", 0+i);
-    $square.addClass("square");
+    $square.addClass("square" + "column" + i%6).addClass("square");
     $board.append($square);
-    $("body").append($board);
+    //column class
 
-    $square.one("click", placeIcon);
+//     make column clickable then look for valid spot
+//     then place
+// valid piece for column 1 is x then increment
+
 };
+
+$(".square" + "column" + 0).one("click", placeIcon);
+$(".square" + "column" + 1).one("click", placeIcon);
+$(".square" + "column" + 2).one("click", placeIcon);
+$(".square" + "column" + 3).one("click", placeIcon);
+$(".square" + "column" + 4).one("click", placeIcon);
+$(".square" + "column" + 5).one("click", placeIcon);
+console.log($(".squarecolumn0"));
 });
